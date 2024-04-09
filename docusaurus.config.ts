f5a53fad7ba4a5e73aaa8fa11cefc45b20853692
Noah Choi <thiszino@gmail.com>
2024-04-09 11:32:34 +0900

    favicon수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
       //     ],
       //   },
       // ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} ZINOAH All rights reserved.`,
     },
     prism: {
       theme: prismThemes.github,
